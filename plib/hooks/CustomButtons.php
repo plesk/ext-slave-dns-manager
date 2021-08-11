b86479f4d6bbf16aeab828812c563ea10ef686a9
Vladislav Baranovskiy <vbaranovskiy@plesk.com>
2021-08-11 16:07:14 +0700

    FEATURE EXTPLESK-2887 Add a link to the extension to the left menu

---
+++ plib/hooks/CustomButtons.php
@@ -0,0 +1,32 @@
+<?php
+// Copyright 1999-2021. Plesk International GmbH. All rights reserved.
+
+/**
+ * Class Modules_SlaveDnsManager_CustomButtons
+ */
+class Modules_SlaveDnsManager_CustomButtons extends pm_Hook_CustomButtons
+{
+    public function getButtons()
+    {
+        if (pm_Session::getClient()->isAdmin()) {
+            $commonParams = [
+                'title'         => pm_Locale::lmsg('leftMenuTitle'),
+                'description'   => pm_Locale::lmsg('leftMenuDescription'),
+                'icon'          => pm_Context::getBaseUrl() . 'icons/32x32.png',
+                'link'          => pm_Context::getActionUrl('index'),
+                'contextParams' => true
+            ];
+            $buttons = [
+                array_merge($commonParams, [
+                    'place' => self::PLACE_ADMIN_NAVIGATION,
+                ]),
+                array_merge($commonParams, [
+                    'place' => self::PLACE_HOSTING_PANEL_NAVIGATION,
+                ]),
+            ];
+            return $buttons;
+        } else {
+            return [];
+        }
+    }
+}
